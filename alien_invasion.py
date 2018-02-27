5780567dfc90c40efd7a3d10adf0752bb333a60f
unknown <0310001936@hx.com>
2018-02-27 08:27:19 +0800

    o

---
+++ alien_invasion.py
@@ -0,0 +1,25 @@
+
+import pygame
+from pygame.sprite import Group
+from settings import Settings
+from ship import Ship
+from alien import Alien
+import game_functions as gf
+
+def run_game():
+    pygame.init()
+    ai_settings=Settings()
+    screen=pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
+   
+    pygame.display.set_caption("外星人入侵")
+    ship=Ship(ai_settings,screen)
+    bullets=Group()
+    alien=Alien(ai_settings,screen)
+    while True:
+        gf.check_events(ai_settings,screen,ship,bullets)
+        ship.update()
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings,screen,ship,alien,bullets)
+       
+        
+run_game()
